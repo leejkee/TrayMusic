4dd01513ca820c53e4af699bf462bafd91874ca4
leejkee <leejykee@yeah.net>
2025-02-12 17:57:19 +0800

    learn end

--- include/tray.h
+++ include/tray.h
@@ -1,7 +1,23 @@
-#include <QSystemTrayIcon>
+#ifndef TRAY_H
+#define TRAY_H
 #include <QDialog>
+#include <QSystemTrayIcon>
 
-class Window: QDialog {
+QT_BEGIN_NAMESPACE
+class QGroupBox;
+class QLabel;
+class QComboBox;
+class QCheckBox;
+class QSpinBox;
+class QLineEdit;
+class QTextEdit;
+class QPushButton;
+class QMenu;
+class QAction;
+
+QT_END_NAMESPACE
+
+class Window final : public QDialog {
     Q_OBJECT
 
 public:
@@ -9,11 +25,43 @@ public:
     void setVisible(bool visible) override;
 
 protected:
-    void closeEvent(QCloseEvent* event) override;
+    void closeEvent(QCloseEvent *event) override;
 private Q_SLOTS:
     void setIcon(int index);
+    void showMessage();
 
 private:
     void createIconGroupBox();
+    void createMessageGroupBox();
+    void createActions();
+    void createTrayIconMenu();
 
+
+    QGroupBox* iconGroupBox;
+    QLabel* iconLabel;
+    QComboBox* iconComboBox;
+    QCheckBox* showIconCheckBox;
+
+    QGroupBox* messageGroupBox;
+    QLabel* messageTypeLabel;
+    QLabel* durationLabel;
+    QLabel* durationInfo;
+    QLabel* titleLabel;
+    QLabel* bodyLabel;
+    QComboBox* messageTypeComboBox;
+    QSpinBox* durationSpinBox;
+    QLineEdit* titleEdit;
+    QTextEdit* bodyEdit;
+    QPushButton* showMessageButton;
+
+    QAction* minimizeAction;
+    QAction* maximizeAction;
+    QAction* restoreAction;
+    QAction* size_800x600_Action;
+    QAction* quitAction;
+
+    QSystemTrayIcon *systemTrayIcon;
+    QMenu *trayIconMenu;
 };
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <QApplication>
-#include <QMainWindow>
-int main(int argc, char** argv) {
+#include <QMessageBox>
 
+#include "tray.h"
+
+int main(int argc, char** argv)
+{
     QApplication app(argc, argv);
-    QMainWindow window;
-    window.show();
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        auto choice = QMessageBox::critical(nullptr, QObject::tr("Systray"),
+            QObject::tr("I couldn't detect any system tray on this system."),
+            QMessageBox::Close | QMessageBox::Ignore);
+        if (choice == QMessageBox::Close)
+            return 1;
+        // Otherwise "lurk": if a system tray is started later, the icon will appear.
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
+    Window w;
+    w.show();
     return app.exec();
 }
 
